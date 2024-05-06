75fdd48d716bb90423ef91448ae7d1c669799673
Porter <you@example.com>
2024-05-06 16:22:49 -0700

    Initial Commit

---
+++ hopefullythelastcprojectnameicomeupwith/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  hopefullythelastcprojectnameicomeupwith
+//
+//  Created by User on 5/6/24.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
